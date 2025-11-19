d8f3bf8c6f6741511edc201b1a20c0c76cebb90d
Mateus A <mateusabreucn@gmail.com>
2025-11-19 20:01:33 -0300

    feat: adicionando detalhe no fundo na seção de serviços

--- app/Servicos.tsx
+++ app/Servicos.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import CardBranco from "./components/CardBranco";
 import IconBadge from "./components/IconBagde";
 
 export default function Servicos() {
   return (
     <CardBranco titulo="Serviços">
-      <div className="w-full grid grid-cols-3 gap-y-40 gap-x-60 justify-items-center">
+      <div className="w-full grid grid-cols-3 gap-y-40 gap-x-60 justify-items-center relative">
+        <Image
+          src="/Icons/Servicos/Ellipse.svg"
+          alt="Detalhe fundo"
+          fill
+          className="object-fill z-0"
+        />
+
         <IconBadge
           icon="/Icons/Servicos/Nacionalidade.svg"
           altText="Ícone de Nacionalidade"
--- app/components/IconBagde.tsx
+++ app/components/IconBagde.tsx
@@ -8,7 +8,7 @@ interface IconBadgeProps {
 
 export default function IconBadge({ icon, altText, titulo }: IconBadgeProps) {
   return (
-    <div className="w-fit flex flex-col gap-y-8 items-center">
+    <div className="w-fit flex flex-col gap-y-8 items-center z-20">
       <div className="rounded-full bg-icon-badge w-48 h-48 flex justify-center items-center">
         <Image src={icon} alt={altText} width={160} height={160} />
       </div>
