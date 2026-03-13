71175bd190fee44c3fcbe221567c4bd1cbc108e1
MateusAbreu <mateusabreucn@gmail.com>
2026-03-13 17:38:17 -0300

    feat: criação da página de serviços de nacionalidade

---
+++ app/nacionalidade/page.tsx
@@ -0,0 +1,23 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import NacionalidadeContent from "../components/Nacionalidade/Content";
+
+export default function NacionalidadePage() {
+  return (
+    <div
+      className="
+        flex flex-col
+        p-6 md:p-10
+        min-h-screen max-w-screen-2xl
+        mx-auto
+        bg-bg-primary
+      "
+    >
+      <Header showNavLinks={false} />
+      <main>
+        <NacionalidadeContent />
+      </main>
+      <Footer />
+    </div>
+  );
+}
