0f4e4d509f80cdd7c604debded05b9cc3eb43ac9
MateusAbreu <mateusabreucn@gmail.com>
2026-03-13 17:37:55 -0300

    feat: criação da página de serviços de empresas

---
+++ app/empresas/page.tsx
@@ -0,0 +1,23 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import EmpresasContent from "../components/Empresas/Content";
+
+export default function EmpresasPage() {
+  return (
+    <div
+      className="
+        flex flex-col
+        p-6 md:p-10
+        min-h-screen max-w-screen-2xl
+        mx-auto
+        bg-bg-primary
+      "
+    >
+      <Header showNavLinks={false} />
+      <main>
+        <EmpresasContent />
+      </main>
+      <Footer />
+    </div>
+  );
+}
