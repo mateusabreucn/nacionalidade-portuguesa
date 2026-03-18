0cb1de23aaab3a8d2bddb0a69f16947cb293b251
MateusAbreu <mateusabreucn@gmail.com>
2026-03-18 16:23:17 -0300

    fix: ajuste de padding vertical

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -7,7 +7,9 @@ export default function Hero() {
       className="
         flex flex-col-reverse lg:flex-row
         justify-between items-center
-        py-12 mx-0 lg:px-[clamp(1rem,calc(15vw-8rem),10rem)]
+        mx-0
+        py-[clamp(1rem,2.8vw,4rem)]
+        lg:px-[clamp(1rem,calc(15vw-8rem),10rem)]
         gap-x-[clamp(1.5rem,calc(3.125vw-0.5rem),2.5rem)]
         gap-[clamp(2rem,5vw,4rem)]
       "
