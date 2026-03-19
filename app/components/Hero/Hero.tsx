db6ac460dd2b2485535a0acfc73d81ec349acc56
MateusAbreu <mateusabreucn@gmail.com>
2026-03-19 10:49:35 -0300

    style: ajuste de padding no hero

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
         flex flex-col-reverse lg:flex-row
         justify-between items-center
         mx-0
-        py-[clamp(1rem,2.8vw,4rem)]
+        py-[clamp(1.5rem,2.8vw,4rem)]
         lg:px-[clamp(1rem,calc(15vw-8rem),10rem)]
         gap-x-[clamp(1.5rem,calc(3.125vw-0.5rem),2.5rem)]
         gap-[clamp(2rem,5vw,4rem)]
